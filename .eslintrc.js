647398e91434df492888a7908d569b1827399344
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-12-01 21:04:10 +0100

    Update .eslintrc.js / Add DB

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier', // Instead of 'plugin:prettier/recommended'
   ],
   root: true,
   env: {
