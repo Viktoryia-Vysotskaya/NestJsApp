5b103bca5d7a5b05b5397d47fc597ddcef690126
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-12-02 10:06:37 +0100

    Add endpoints 'delete', 'post', 'put' for orders / DOT / Prefix api

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  Post,
+  Put,
+  Body,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { ParseUUIDPipe, NotFoundException } from '@nestjs/common';
+import { CreateOrderDTO } from './dtos/create-order.dto';
+import { UpdateOrderDTO } from './dtos/update-order.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -12,9 +22,34 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const order = await this.ordersService.getById(id);
+  getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const order = this.ordersService.getById(id);
     if (!order) throw new NotFoundException('Order not found');
     return order;
+  }
+
+  @Delete('/:id')
+  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!this.ordersService.getById(id))
+      throw new NotFoundException('Order not found');
+    this.ordersService.deleteById(id);
+    return { success: true };
+  }
+
+  @Post('/')
+  public create(@Body() orderData: CreateOrderDTO) {
+    return this.ordersService.create(orderData);
+  }
+
+  @Put('/:id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() orderData: UpdateOrderDTO,
+  ) {
+    if (!this.ordersService.getById(id))
+      throw new NotFoundException('Order not found');
+
+    this.ordersService.updateById(id, orderData);
+    return { success: true };
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 import { db, Order } from './../db';
 
 @Injectable()
@@ -9,5 +10,24 @@ export class OrdersService {
 
   public getById(id: Order['id']): Order | null {
     return db.orders.find((p) => p.id === id);
+  }
+
+  public deleteById(id: Order['id']): void {
+    db.orders = db.orders.filter((p) => p.id !== id);
+  }
+
+  public create(orderData: Omit<Order, 'id'>): Order {
+    const newOrder = { ...orderData, id: uuidv4() };
+    db.orders.push(newOrder);
+    return newOrder;
+  }
+  
+  public updateById(id: Order['id'], orderData: Omit<Order, 'id'>): void {
+    db.orders = db.orders.map((p) => {
+      if (p.id === id) {
+        return { ...p, ...orderData };
+      }
+      return p;
+    });
   }
 }
